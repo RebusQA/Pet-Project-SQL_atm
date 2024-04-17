0d10593d244fad2c982eb97d9e84fb1fcb3a761b
RebusQA <RebusQAE@gmail.com>
2024-04-17 12:50:31 +0300

    Create new user

--- atm.py
+++ atm.py
@@ -7,7 +7,7 @@ class ATM():
     def atm_logic(self):
 
         SQL_atm.create_table()
-
+        # SQL_atm.insert_users((7777, 3333, 10000))
 
 start = ATM()
 start.atm_logic()
--- sql_qwery.py
+++ sql_qwery.py
@@ -8,10 +8,19 @@ class SQL_atm:
     def create_table():
         with sqlite3.connect("atm.db") as db:
             cur = db.cursor()
-            cur.execute("""CREATE TABLE IF NOT EXISTS User_data(
+            cur.execute("""CREATE TABLE IF NOT EXISTS Users_data(
             UserID INTEGER PRIMARY KEY AUTOINCREMENT,
             Number_card INTEGER NOT NULL,
             Pin_code INTEGER NOT NULL,
             Balance INTEGER NOT NULL);            
             """)
             print("Создание таблицы User_data")
+
+
+    """Создание нового пользователя"""
+    @staticmethod
+    def insert_users(data_users):
+        with sqlite3.connect("atm.db") as db:
+            cur = db.cursor()
+            cur.execute("""INSERT INTO Users_data (Number_card, Pin_code, Balance) VALUES(?, ?,  ?);""", data_users)
+            print("Создание нового пользователя")
