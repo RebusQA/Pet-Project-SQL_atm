bd79617e277360abffb40b5fe28cda26ab2470d8
RebusQA <RebusQAE@gmail.com>
2024-04-17 15:05:43 +0300

    Checking card number. Testing scripts.

--- atm.py
+++ atm.py
@@ -8,6 +8,14 @@ class ATM():
 
         SQL_atm.create_table()
         # SQL_atm.insert_users((7777, 3333, 10000))
+        """Проверяю что метод ввода карты работает"""
+        number_card = input("Введите номер карты: ")
+
+        while True:
+            if SQL_atm.input_card(number_card):
+                print("7777")
+            else:
+                break
 
 start = ATM()
 start.atm_logic()
--- sql_qwery.py
+++ sql_qwery.py
@@ -3,7 +3,7 @@ import sqlite3
 
 class SQL_atm:
 
-    """Создание таблицы User_data"""
+    """Создание таблицы Users_data"""
     @staticmethod
     def create_table():
         with sqlite3.connect("atm.db") as db:
@@ -14,7 +14,7 @@ class SQL_atm:
             Pin_code INTEGER NOT NULL,
             Balance INTEGER NOT NULL);            
             """)
-            print("Создание таблицы User_data")
+            print("Создание таблицы Users_data")
 
 
     """Создание нового пользователя"""
@@ -24,3 +24,23 @@ class SQL_atm:
             cur = db.cursor()
             cur.execute("""INSERT INTO Users_data (Number_card, Pin_code, Balance) VALUES(?, ?,  ?);""", data_users)
             print("Создание нового пользователя")
+
+
+    """Ввод и проверка карты"""
+    @staticmethod
+    def input_card(number_card):
+        try:
+            with sqlite3.connect("atm.db") as db:
+                cur = db.cursor()
+                cur.execute(f"""SELECT Number_card FROM Users_data WHERE Number_card = {number_card}""")
+                result_card = cur.fetchone()
+
+                """Логика проверки номера карты"""
+                if result_card is None:
+                    print("Введён неизвестный номер карты")
+                    return False
+                else:
+                    print(f"Введён номер карты: {number_card}")
+                    return True
+        except:
+            print("Введён неизвестный номер карты")
